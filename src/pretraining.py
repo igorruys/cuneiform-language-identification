412944756bdd447b6cfd92cf780d39844fc083cc
Igor Ruys <igorruys@gmail.com>
2023-01-27 02:25:48 -0300

    Added a function for generating chunked datasets for mlm in data

--- src/pretraining.py
+++ src/pretraining.py
@@ -11,9 +11,20 @@ args = parser.parse_args()
 
 def tokenize_function(example):
     '''
-    Creates a tokenized dataset.
+    Function to be used during the tokenization of the dataset. The resulting tokenized
+    dataset will have a extra column 'input_ids' with tokens ids corresponding to the 
+    cuneiform charcters (and white spaces) in example.
+    -------------------------------------------------------------
+    Args:
+        example: dictionary-like object that represents one example of the dataset,
+        that is, a single pair "text","label" (Ex: {'text':..., 'label':...}).
+    Returns:
+        A dictionary with a single key input_ids, whose value is a list of token ids
+        corresponding to the characters, given example's 'text' key.
     '''
 
+    #output is a dictionary with keys "input_ids", "token_type_ids" and
+    #"attention mask", whose values are lists of integers
     output = tokenizer(example["text"], truncation=True, max_length=max_len)
     input_batch = []
     for token_id in output['input_ids']:
@@ -26,9 +37,12 @@ dataset = load_from_disk('../data/datasets/cuneiform/')
 #Loading tokenizer
 tokenizer = AutoTokenizer.from_pretrained('../tokenizers/tokenizer/')
 vocab_size = tokenizer.vocab_size
-max_len = tokenizer.model_max_length
 
-#Tokenized dataset
+#Since only 0,01% of all data has more than 64 cuneiform characters (or 2*64 -1, if we count the white spaces),
+#we set max_len to this value
+max_len = 2*64
+
+#Tokenized dataset that has a single column "input_ids", that is a list of tokens ids lists.
 tokenized_dataset = dataset.map(tokenize_function, batched=True, remove_columns=dataset["train"].column_names)
 
 #Data collator
